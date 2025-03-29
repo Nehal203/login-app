643ebb911f2557da45099fd4597a0959a2f3584c
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2025-03-29 17:17:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";  
 import Login from "./components/Login";      
 import Signup from "./components/Signup";   
-import AccountSettings from "./components/AccountSettings";  // ✅ Naya Component Import Kiya
+import AccountSettings from "./components/AccountSettings";  
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/account-settings" element={<AccountSettings />} />  {/* ✅ AccountSettings ka Route */}
+        <Route path="/account-settings" element={<AccountSettings />} /> 
       </Routes>
     </Router>
   );
